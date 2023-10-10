4f6b1b0c48ea4be9675e5091e0e5f27785817d39
Jean Martins <jeanmrtns4@gmail.com>
2023-10-10 20:05:40 -0300

    feat: setup jpa database

--- src/main/java/com/jeanmrtns/todolist/user/User.java
+++ src/main/java/com/jeanmrtns/todolist/user/User.java
@@ -1,32 +1,31 @@
 package com.jeanmrtns.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_users")
 public class User {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    
+    @Column(unique = true)
     private String username;
+
+    @Column(unique = true)
     private String email;
     private String password;
 
-    public User(String username, String email, String password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
-    
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
