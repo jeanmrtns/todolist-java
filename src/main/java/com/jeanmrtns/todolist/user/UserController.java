bbd458cdab21e3f426d36126ba948c8db5940398
Jean Martins <jeanmrtns4@gmail.com>
2023-10-10 20:06:43 -0300

    feat: validate user creation

--- src/main/java/com/jeanmrtns/todolist/user/UserController.java
+++ src/main/java/com/jeanmrtns/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package com.jeanmrtns.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping()
-    public String createUser(@RequestBody User user) {
-        return user.getEmail();
+    public ResponseEntity createUser(@RequestBody User user) {
+        User userAlreadyExists = this.userRepository.findByEmail(user.getEmail());
+
+        if (userAlreadyExists != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists");
+        }
+
+        User createdUser = this.userRepository.save(user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
     }
 }
